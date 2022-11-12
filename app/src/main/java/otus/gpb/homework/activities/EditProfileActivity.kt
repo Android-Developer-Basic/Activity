08ebab66108607414c93df423afe991ec473d728
viktoriurkov <VVYurkov@alfabank.ru>
2022-11-12 18:25:02 +0300

    feat(*): add click listener and dialog

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -6,15 +6,22 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
+    private val alertChoices = arrayOf(
+        resources.getString(R.string.choose_photo),
+        resources.getString(R.string.make_photo)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
-        imageView = findViewById(R.id.imageview_photo)
+        imageView = findViewById<ImageView?>(R.id.imageview_photo).apply {
+            setOnClickListener { showAlertDialog() }
+        }
 
         findViewById<Toolbar>(R.id.toolbar).apply {
             inflateMenu(R.menu.menu)
@@ -28,6 +35,19 @@ class EditProfileActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun showAlertDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(resources.getString(R.string.alert_title))
+            .setItems(alertChoices) { _, index: Int ->
+                    when (index) {
+                        0 -> {}
+                        1 -> {}
+                        else -> {}
+                    }
+            }
+            .show()
     }
 
     /**
