be09884fa06670e5f9e23a91951b9e0fd34b1a9f
viktoriurkov <VVYurkov@alfabank.ru>
2022-11-16 22:14:11 +0300

    feat(*): third part

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -1,35 +1,65 @@
 package otus.gpb.homework.activities
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.NameNotFoundException
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import otus.gpb.homework.activities.dto.User
 
-class EditProfileActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity(), OnClickListener {
 
     private lateinit var imageView: ImageView
     private lateinit var alertChoices: Array<String>
+    private lateinit var firstName: TextView
+    private lateinit var lastName: TextView
+    private lateinit var age: TextView
+    private lateinit var editBtn: Button
+    private lateinit var user: User
+    private lateinit var uri: Uri
 
     private val requestCameraPermissionLauncher = registerForActivityResult(
         RequestPermission(),
         ::permissionResultHandler
     )
 
-    private val getPictureFromLocalLibrary = registerForActivityResult(GetContent()) {
-        uri -> if (uri !== null) populateImage(uri)
+    private val getFillForm = registerForActivityResult(CustomActivityResultContract()) {
+        if (it !== null) {
+            user = it
+            firstName.text = it.firstName
+            lastName.text = it.lastName
+            age.text = it.age
+        }
+    }
+
+    private val getPictureFromLocalLibrary = registerForActivityResult(GetContent()) { uri ->
+        if (uri !== null) {
+            this.uri = uri
+            populateImage(uri)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
-        imageView = findViewById<ImageView?>(R.id.imageview_photo).apply {
-            setOnClickListener { showAlertDialog() }
-        }
+
+        imageView = findViewById(R.id.imageview_photo)
+        firstName = findViewById(R.id.textview_name)
+        lastName = findViewById(R.id.textview_surname)
+        age = findViewById(R.id.textview_age)
+        editBtn = findViewById(R.id.button4)
 
         alertChoices = arrayOf(
             resources.getString(R.string.choose_photo),
@@ -58,7 +88,8 @@ class EditProfileActivity : AppCompatActivity() {
             if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
                 MaterialAlertDialogBuilder(this)
                     .setTitle(R.string.why_we_need_permission)
-                    .setPositiveButton(R.string.give_permission
+                    .setPositiveButton(
+                        R.string.give_permission
                     ) { _, _ -> requestCameraPermissionLauncher.launch(android.Manifest.permission.CAMERA) }
                     .setNegativeButton(R.string.cancel_give_permission, null)
                     .show()
@@ -70,11 +101,11 @@ class EditProfileActivity : AppCompatActivity() {
         MaterialAlertDialogBuilder(this)
             .setTitle(resources.getString(R.string.alert_title))
             .setItems(alertChoices) { _, index: Int ->
-                    when (index) {
-                        0 -> getPictureFromLocalLibrary.launch("image/*")
-                        1 -> requestCameraPermissionLauncher.launch(android.Manifest.permission.CAMERA)
-                        else -> {}
-                    }
+                when (index) {
+                    0 -> getPictureFromLocalLibrary.launch("image/*")
+                    1 -> requestCameraPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+                    else -> {}
+                }
             }
             .show()
     }
@@ -88,6 +119,31 @@ class EditProfileActivity : AppCompatActivity() {
     }
 
     private fun openSenderApp() {
-        TODO("В качестве реализации метода отправьте неявный Intent чтобы поделиться профилем. В качестве extras передайте заполненные строки и картинку")
+        val telegramPackageName = "org.telegram.messenger"
+        try {
+            this.packageManager.getPackageInfo(
+                telegramPackageName,
+                PackageManager.GET_ACTIVITIES
+            )
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                setPackage(telegramPackageName)
+                putExtra("Extra", user)
+                putExtra("Picture", uri)
+            }
+            startActivity(intent)
+        } catch (e: NameNotFoundException) {
+            Toast.makeText(
+                this,
+                "No Telegram app found on system",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.button4 -> getFillForm.launch("")
+            R.id.imageview_photo -> showAlertDialog()
+        }
     }
 }
