c8193123806018b4e26c03406a004d2141bc2d44
Irina Keil <irina.keil@gmail.com>
2022-12-16 02:13:41 +0300

    build: Homework 3

---
+++ app/src/main/java/otus/gpb/homework/activities/UserDTO.kt
@@ -0,0 +1,12 @@
+package otus.gpb.homework.activities
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class UserDTO(
+    val name: String,
+    val surname: String,
+    val age: Int
+) : Parcelable
