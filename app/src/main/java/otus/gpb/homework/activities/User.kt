5cd6ff0621d2020118072b56c2a769c66548d909
Dmitriy <dimam-78@mail.ru>
2023-01-11 20:32:53 +0300

    Activity_03 finish

---
+++ app/src/main/java/otus/gpb/homework/activities/User.kt
@@ -0,0 +1,7 @@
+package otus.gpb.homework.activities
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(val name: String, val surname: String, val age: String) : Parcelable
