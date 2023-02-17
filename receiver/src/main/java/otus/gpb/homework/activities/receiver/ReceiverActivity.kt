bdb2624a2dd65b475e6437df54a73b370cf732fb
78trav <trav78@yandex.ru>
2023-02-17 18:58:34 +0300

    activity homework 2 from trav78

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,6 +1,10 @@
 package otus.gpb.homework.activities.receiver
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReceiverActivity : AppCompatActivity() {
@@ -8,5 +12,17 @@ class ReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val descriptionTextView = findViewById<TextView>(R.id.descriptionTextView)
+        val yearTextView = findViewById<TextView>(R.id.yearTextView)
+        val posterImageView = findViewById<ImageView>(R.id.posterImageView)
+
+        intent.extras?.let {
+            titleTextView.text = it.getString("title", ":( no title")
+            descriptionTextView.text = it.getString("description", ":( no description")
+            yearTextView.text = it.getString("year", ":( no year")
+            posterImageView.setImageDrawable(getDrawable(R.drawable.interstellar))
+        }
     }
 }
