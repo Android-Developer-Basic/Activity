594153f578fc038a4f69e1b72dbfe0a03e193d0f
Igor Yagodin <igornn95@gmail.com>
2023-02-26 02:42:01 +0300

    init solution

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,12 +1,21 @@
 package otus.gpb.homework.activities.receiver
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class ReceiverActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        findViewById<TextView>(R.id.titleTextView).text = intent.getStringExtra("title")
+        findViewById<TextView>(R.id.yearTextView).text = intent.getStringExtra("year")
+        findViewById<TextView>(R.id.descriptionTextView).text = intent.getStringExtra("description")
+        findViewById<ImageView>(R.id.posterImageView).setImageDrawable(getDrawable(R.drawable.interstellar))
     }
 }
