72f4943c20fb5d22187e5bfd24a91436fcb707c7
Tonius <toniuswar@gmail.com>
2023-03-02 02:33:44 +0300

    Пофиксил. Запрос разрешений стал более адекватным.

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -6,6 +6,7 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
@@ -36,76 +37,14 @@ class EditProfileActivity : AppCompatActivity() {
     //Переменнные класса
     private lateinit var imageView: ImageView
     private val userProfile:User = User()
-    private var isFirstClickOnImg = true
-    private var isFirstAlertDialogCall = true
-    private var isCameraAccess = false
+    private var isFirstCameraPermissionDialog = true
+    private var isFirstStoragePermissionDialog = true
     private lateinit var nameTextView:TextView
     private lateinit var surnameTextView:TextView
     private lateinit var ageTextView:TextView
+    private val cameraP = Manifest.permission.CAMERA
+    private val storageP = Manifest.permission.WRITE_EXTERNAL_STORAGE
 
-    //Контракты:
-
-    //EditProfile <-> FillFromActivity:
-    private val userContract = registerForActivityResult(DataContract()){
-        if(it == null) {}
-        else {
-            val resultArray = it.split(DATA_STRING_SEPARATOR)
-            userProfile.name = resultArray[0]
-            userProfile.surname = resultArray[1]
-            userProfile.age = resultArray[2]
-            setProfileProperties()
-        }
-    }
-
-    //Получение изображений из галереи:
-    private val getImgContract = registerForActivityResult(ActivityResultContracts.GetContent()){
-        it?.let {
-            userPhotoUri = it
-            populateImage(it)
-        }
-    }
-
-    //Камера:
-    private val camera = registerForActivityResult(CameraContract()){
-            populateImage(Uri.fromFile(imgFile))
-
-    }
-
-    //Память
-    private  val writeStoragePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){
-        when{
-            it -> setCat()
-
-            !shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE) ->{
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                    val uri = Uri.fromParts("package", packageName, null)
-                    data = uri
-                }
-                startActivity(intent)
-            }
-
-            else ->{}
-
-        }
-    }
-
-    //Разрешение на использование камеры:
-    private val permissionForCamera = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-        when {
-            it -> {
-                showChoice()
-                isCameraAccess = true
-            }
-            !shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                    val uri = Uri.fromParts("package", packageName, null)
-                    data = uri
-                }
-                startActivity(intent)
-            }
-            else -> isFirstClickOnImg = false
-        }
-    }
     //OnCreate fun:
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -132,6 +71,77 @@ class EditProfileActivity : AppCompatActivity() {
         }
     }
 
+    //Контракты:
+
+    //EditProfile <-> FillFromActivity:
+    private val userContract = registerForActivityResult(DataContract()){
+        it?.let {
+            val resultArray = it.split(DATA_STRING_SEPARATOR)
+            userProfile.name = resultArray[0]
+            userProfile.surname = resultArray[1]
+            userProfile.age = resultArray[2]
+            setProfileProperties()
+        }
+
+    }
+
+    //Получение изображений из галереи:
+    private val getImgContract = registerForActivityResult(ActivityResultContracts.GetContent()){
+        it?.let {
+            userPhotoUri = it
+            populateImage(it)
+        }
+    }
+
+    //Камера:
+    private val camera = registerForActivityResult(CameraContract()){
+            populateImage(Uri.fromFile(imgFile))
+
+    }
+
+    // Разрешение на получение Uri котейки, если  версия SDK - 28 и меньше"
+    private  val writeStoragePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        if(it) setCat()
+        else{
+            if(!isFirstCameraPermissionDialog){
+                showAlertDialog(isFirstStoragePermissionDialog, storageP)
+            }
+        }
+    }
+    // Разрешение на доступ к камере
+    private val cameraPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        if(it) showChoice()
+        else{
+            if(!isFirstCameraPermissionDialog){
+                showAlertDialog(isFirstCameraPermissionDialog, cameraP)
+            }
+        }
+    }
+
+    //Проверка разрешений:
+   private fun checkPermission(permission:String){
+       when{
+           ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED ->{
+               when(permission){
+                   cameraP -> showChoice()
+                   storageP -> setCat()
+               }
+           }
+           shouldShowRequestPermissionRationale(permission) -> {
+               when (permission) {
+                   cameraP -> showAlertDialog(isFirstCameraPermissionDialog, permission)
+                   storageP -> showAlertDialog(isFirstStoragePermissionDialog, permission)
+               }
+           }
+           else ->{
+               when(permission){
+                   cameraP -> cameraPermission.launch(permission)
+                   storageP -> writeStoragePermission.launch(permission)
+               }
+           }
+       }
+   }
+
     //Функции:
 
     //Заполнение данных профиля:
@@ -142,6 +152,7 @@ class EditProfileActivity : AppCompatActivity() {
         ageTextView.text = userProfile.age + agePostfix
     }
 
+    //Файл для хранения фото с камеры
     @SuppressLint("SimpleDateFormat")
     private fun createImgFile(): File?{
         val dateStamp = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
@@ -159,27 +170,29 @@ class EditProfileActivity : AppCompatActivity() {
     }
 
     //Клик по imageView:
-    private fun clickOnTakePhoto(isFirst: Boolean){
-        if(isCameraAccess){
-            permissionForCamera.launch(Manifest.permission.CAMERA)
-            return
-        }
-        if(isFirst) permissionForCamera.launch(Manifest.permission.CAMERA)
-        else showAlertDialog(isFirstAlertDialogCall)
+    private fun clickOnTakePhoto(){
+        checkPermission(cameraP)
+
     }
 
     //Алерт-диалог, вызываемый при запросе разрешения на использование камеры:
-    private fun showAlertDialog(isFirst:Boolean = true){
+    private fun showAlertDialog(isFirst:Boolean, permission:String){
         val alert = AlertDialog.Builder(this)
+        var alertMessage = -1
+        when(permission){
+            cameraP -> {alertMessage = R.string.text_for_alert_dialog
+            }
+            storageP ->{alertMessage = R.string.text_for_alert_dialog2}
+        }
         if(isFirst) {
             alert.setTitle(R.string.alert)
-            alert.setMessage(R.string.text_for_alert_dialog)
+            alert.setMessage(alertMessage)
             alert.setNegativeButton(R.string.deny) { dialog, _ ->
                 dialog.dismiss()
             }
 
             alert.setPositiveButton(R.string.access) { _, _ ->
-                permissionForCamera.launch(Manifest.permission.CAMERA)
+               cameraPermission.launch(permission)
             }
         }
         else alert.setPositiveButton(R.string.settings){ _, _ ->
@@ -189,8 +202,13 @@ class EditProfileActivity : AppCompatActivity() {
 
             }
             startActivity(intent)
+
         }
-        isFirstAlertDialogCall = false
+        when(permission) {
+            cameraP -> isFirstCameraPermissionDialog = false
+            storageP -> isFirstStoragePermissionDialog = false
+        }
+
         alert.show()
     }
 
@@ -206,7 +224,7 @@ class EditProfileActivity : AppCompatActivity() {
 
         //Поставить котейку на аватар
         alert.setNegativeButton(R.string.cat){_, _ ->
-            if(Build.VERSION.SDK_INT <= 28) writeStoragePermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            if(Build.VERSION.SDK_INT <= 28) checkPermission(storageP)
             else setCat()
 
         }
@@ -229,7 +247,7 @@ class EditProfileActivity : AppCompatActivity() {
             val selectedText = parent.getItemAtPosition(position)
             when(selectedText.toString()){
                 resources.getString(R.string.take_photo) -> {
-                    clickOnTakePhoto(isFirstClickOnImg)
+                    clickOnTakePhoto()
                     dialog.dismiss()
                 }
                 resources.getString(R.string.chose_photo) -> {
