2c9c9e2385ea8d7cc62286555e5cec92c8de0ac0
Pavel Iohin <pavel@iohin.ru>
2023-03-02 19:08:24 +0300

    fixed email sending

--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -18,33 +18,39 @@ class SenderActivity : AppCompatActivity() {
                 Uri.parse("geo:0,0?q=Restaurants")
             val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
             mapIntent.setPackage("com.google.android.apps.maps")
-            try {
+            mapIntent.resolveActivity(packageManager)?.let {
                 startActivity(mapIntent)
-            } catch (e: ActivityNotFoundException) {
+            } ?: run {
                 Toast.makeText(this, R.string.google_maps_not_installed, Toast.LENGTH_SHORT).show()
             }
         }
         findViewById<Button>(R.id.activity_sender_send_email).setOnClickListener {
-            try {
-                startActivity(Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:android@otus.ru")).apply {
-                    putExtra(Intent.EXTRA_SUBJECT, "Subject")
-                    putExtra(Intent.EXTRA_TEXT, "Body")
-                })
-            } catch (e: ActivityNotFoundException) {
+            val email = "android@otus.ru"
+            val subject = "Subject"
+            val body = "Body"
+            val urlString = "mailto:" + Uri.encode(email) + "?subject=" + Uri.encode(subject) + "&body=" + Uri.encode(body)
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse(urlString)).apply {
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, body)
+            }
+            intent.resolveActivity(packageManager)?.let {
+                startActivity(intent)
+            } ?: run {
                 Toast.makeText(this, R.string.email_clients_not_installed, Toast.LENGTH_SHORT).show()
             }
         }
         findViewById<Button>(R.id.activity_sender_open_receiver).setOnClickListener {
-            try {
-                startActivity(Intent(Intent.ACTION_SEND).apply {
-                    setDataAndType(Uri.parse("some data"), "text/plain")
-                    addCategory( Intent.CATEGORY_DEFAULT)
-                    putExtra("title", "Славные парни")
-                    putExtra("year", "2016")
-                    putExtra("description", "Что бывает, когда напарником брутального костолома становится субтильный лопух? Наемный охранник Джексон Хили и частный детектив Холланд Марч вынуждены работать в паре, чтобы распутать плевое дело о пропавшей девушке, которое оборачивается преступлением века. Смогут ли парни разгадать сложный ребус, если у каждого из них – свои, весьма индивидуальные методы.")
-                })
-            } catch (e: ActivityNotFoundException) {
-                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                addCategory( Intent.CATEGORY_DEFAULT)
+                putExtra("title", "Славные парни")
+                putExtra("year", "2016")
+                putExtra("description", "Что бывает, когда напарником брутального костолома становится субтильный лопух? Наемный охранник Джексон Хили и частный детектив Холланд Марч вынуждены работать в паре, чтобы распутать плевое дело о пропавшей девушке, которое оборачивается преступлением века. Смогут ли парни разгадать сложный ребус, если у каждого из них – свои, весьма индивидуальные методы.")
+            }
+            intent.resolveActivity(packageManager)?.let {
+                startActivity(intent)
+            } ?: run {
+                Toast.makeText(this, R.string.receiver_not_installed, Toast.LENGTH_SHORT).show()
             }
         }
     }
