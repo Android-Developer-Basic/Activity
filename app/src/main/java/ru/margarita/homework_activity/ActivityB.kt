20fe3848b217008873d562df789696846c62de5c
daeneris <margarita.dedova@gmail.com>
2023-04-20 13:47:33 +0300

    Activity1 Margarita Dedova

---
+++ app/src/main/java/ru/margarita/homework_activity/ActivityB.kt
@@ -0,0 +1,23 @@
+package ru.margarita.homework_activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import otus.gpb.homework.activities.R
+
+class ActivityB:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+        val button = findViewById<Button>(R.id.button)
+
+        //2. По клику на кнопку “Open ActivityC” запустите ActivityC в том же стеке, в котором расположена ActivityB
+        button.setOnClickListener{
+            val intent = Intent (this, ActivityC::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+}
---
+++ app/src/main/java/ru/margarita/homework_activity/ActivityC.kt
@@ -0,0 +1,45 @@
+package ru.margarita.homework_activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import otus.gpb.homework.activities.R
+
+class ActivityC: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c)
+        val buttonOpenA = findViewById<Button>(R.id.buttonOpenA)
+        val buttonOpenD = findViewById<Button>(R.id.buttonOpenD)
+        val buttonCloseC = findViewById<Button>(R.id.buttonCloseC)
+        val buttonCloseStack = findViewById<Button>(R.id.buttonCloseStack)
+
+        buttonOpenA.setOnClickListener {
+            val intent = Intent(this, ActivityA::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+        // По клику на кнопку “Open ActivityD” запустите ActivityD в том же стеке, где расположены ActivityB и ActivityC, при этом завершите все предыдущие Activity, которые находятся в текущем стеке
+        buttonOpenD.setOnClickListener {
+            val intent = Intent(this, ActivityD::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+
+        }
+
+        // По клику на кнопку “CloseActivityC”, завершите ActivityC, и перейдите на предыдущий экран в стеке
+        buttonCloseC.setOnClickListener {
+            finish();
+        }
+
+        //По клику на кнопку “Close Stack” завершите текущий стек, в котором находятся ActivityB и ActivityC, и перейдите на ActivityA
+        buttonCloseStack.setOnClickListener {
+            val intent = Intent(this, ActivityA::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
+        }
+    }
+}
