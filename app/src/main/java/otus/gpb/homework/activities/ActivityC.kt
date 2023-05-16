7c9507cec04889a21677e12aa56df7dd8aeb4330
Aleksei Borodulia <a.borodulya@yahoo.com>
2023-05-16 23:47:52 +0300

    Добавил Activity A, Activity B, Activivty C, Activity D Добавил кнопки и логику для вызова активити.

--- app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
@@ -11,23 +11,23 @@ class ActivityC : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_c)
 
-        val buttonCloseMe = findViewById<Button>(R.id.buttoncloseactivity)
+        val buttonCloseMe = findViewById<Button>(R.id.close_activity_button)
         buttonCloseMe.setOnClickListener {
             Intent(this, ActivityC::class.java)
             this.finish()
         }
-        val buttona = findViewById<Button>(R.id.button_a)
+        val buttona = findViewById<Button>(R.id.a_activity_button)
         buttona.setOnClickListener {
             val intent = Intent(this, ActivityA::class.java)
             startActivity(intent)
         }
-        val buttond = findViewById<Button>(R.id.button_d)
+        val buttond = findViewById<Button>(R.id.d_activity_button)
         buttond.setOnClickListener {
             val intent = Intent(this, ActivityD::class.java)
             intent.flags = FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
-        val buttonCloseStack = findViewById<Button>(R.id.buttonclosestack)
+        val buttonCloseStack = findViewById<Button>(R.id.close_stack_button)
         buttonCloseStack.setOnClickListener {
             finishAffinity()
         }
