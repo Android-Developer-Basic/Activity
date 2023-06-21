2e1cadf8b3796dadb73d6e6c04c436a384d311dd
ARybachek <email>
2023-06-21 17:06:27 +0300

    Выполнено ДЗ.

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,12 +1,19 @@
 package otus.gpb.homework.activities.receiver
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import otus.gpb.homework.activities.receiver.databinding.ActivityReceiverBinding
 
 class ReceiverActivity : AppCompatActivity() {
-
+    val binding : ActivityReceiverBinding by lazy { ActivityReceiverBinding.inflate(layoutInflater) }
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_receiver)
+        setContentView(binding.root)
+        binding.titleTextView.text = intent.getStringExtra("title")
+        binding.yearTextView.text = intent.getStringExtra("year")
+        binding.descriptionTextView.text = intent.getStringExtra("description")
+        binding.posterImageView.setImageDrawable(applicationContext.getDrawable(R.drawable.niceguys))
     }
 }
