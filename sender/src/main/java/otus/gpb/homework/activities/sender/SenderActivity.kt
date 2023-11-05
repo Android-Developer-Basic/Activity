6291038d5584914d7d33f2549416fdc5dc4b6cf6
Сычев Михаил Валерьевич <msychev@mts.ru>
2023-11-05 19:06:23 +0300

    add homework for activity lesson 2

---
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -0,0 +1,53 @@
+package otus.gpb.homework.activities.sender
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SenderActivity : AppCompatActivity(R.layout.sender_activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        val buttonToGoogleMaps = findViewById<Button>(R.id.buttonToGoogleMaps)
+        buttonToGoogleMaps.setOnClickListener {
+            val nearbyRestaurantsUri = Uri.parse("geo:0,0?q=Рестораны")
+            val mapIntent = Intent(Intent.ACTION_VIEW, nearbyRestaurantsUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+
+        val buttonSendEmail = findViewById<Button>(R.id.buttonSendEmail)
+        buttonSendEmail.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:android@otus.ru"))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Директору Отуса")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Что делаете сегодня вечером?")
+            startActivity(emailIntent)
+            }
+
+        val buttonOpenReceiver = findViewById<Button>(R.id.buttonOpenReceiver)
+        val niceGuy = Payload(
+            "Хорошие Парни",
+            "2020",
+            "Тестовое описание"
+        )
+        buttonOpenReceiver.setOnClickListener {
+            val openReceiverIntent = Intent(Intent.ACTION_SEND)
+                .addCategory(Intent.CATEGORY_DEFAULT)
+                .setType("text/plain")
+                .putExtra("title", niceGuy.title)
+                .putExtra("year", niceGuy.year)
+                .putExtra("description", niceGuy.description)
+
+            startActivity(openReceiverIntent)
+        }
+    }
+
+}
