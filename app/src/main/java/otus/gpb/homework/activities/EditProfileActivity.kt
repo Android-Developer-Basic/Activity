3ecd99e5d1e2b30102fc8da54f3ca53fa06217af
Artem Nagorny <agnagor3@mts.ru>
2023-11-10 00:41:24 +0300

    task 3 solution

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -6,7 +6,10 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +18,11 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 
 class EditProfileActivity : AppCompatActivity() {
+    private var imageUri: Uri? = null
+
+    private val tvName: TextView by lazy { findViewById(R.id.textview_name) }
+    private val tvSurname: TextView by lazy { findViewById(R.id.textview_surname) }
+    private val tvAge: TextView by lazy { findViewById(R.id.textview_age) }
 
     private lateinit var imageView: ImageView
 
@@ -64,10 +72,46 @@ class EditProfileActivity : AppCompatActivity() {
                 }
             }
         }
+        findViewById<Button>(R.id.button4).setOnClickListener {
+            openFillForm()
+        }
 
         imageView.setOnClickListener {
             showProfileImageChangerDialog()
         }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_FILL_FORM && data != null) {
+            FillFormActivity.getEditDataFromIntent(data)?.let {
+                setEditedDataToViews(it)
+            }
+        }
+    }
+
+    private fun setEditedDataToViews(editData: FillFormActivity.EditData) {
+        tvName.text = editData.name
+        tvSurname.text = editData.surname
+        tvAge.text = editData.age
+    }
+
+    private fun getEditDataFromViews(): FillFormActivity.EditData = FillFormActivity.EditData(
+        name = tvName.text.toString(),
+        surname = tvSurname.text.toString(),
+        age = tvAge.text.toString(),
+    )
+
+    private fun openFillForm() {
+        val data = getEditDataFromViews()
+        val intent = FillFormActivity.getIntentToStartForResult(this, data)
+
+        startActivityForResult(intent, REQUEST_CODE_FILL_FORM)
     }
 
     private fun showProfileImageChangerDialog() {
@@ -127,6 +171,7 @@ class EditProfileActivity : AppCompatActivity() {
                 null,
             )
         }
+
         startActivity(intent)
     }
 
@@ -150,10 +195,45 @@ class EditProfileActivity : AppCompatActivity() {
      */
     private fun populateImage(uri: Uri) {
         val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri))
+
         imageView.setImageBitmap(bitmap)
+        imageUri = uri
     }
 
     private fun openSenderApp() {
-        TODO("В качестве реализации метода отправьте неявный Intent чтобы поделиться профилем. В качестве extras передайте заполненные строки и картинку")
+        // В качестве реализации метода отправьте неявный Intent чтобы поделиться профилем. В качестве extras передайте заполненные строки и картинку
+        val appName = "org.telegram.messenger"
+
+        if (isAppAvailable(this, appName)) {
+            val intent = createIntentForSenderApp(appName)
+
+            startActivity(Intent.createChooser(intent, "Share with"))
+        } else {
+            Toast.makeText(this, "Telegram не установлен!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun createIntentForSenderApp(appName: String): Intent {
+        val text = getTextForSenderApp()
+
+        return Intent(Intent.ACTION_SEND).apply {
+            putExtra(Intent.EXTRA_TEXT, text)
+            putExtra(Intent.EXTRA_STREAM, imageUri)
+            type = "image/*"
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            setPackage(appName)
+        }
+    }
+
+    private fun getTextForSenderApp(): String {
+        return listOf(
+            tvName.text.toString(),
+            tvSurname.text.toString(),
+            tvAge.text.toString(),
+        ).joinToString(" ").trim()
+    }
+
+    companion object {
+        private const val REQUEST_CODE_FILL_FORM = 1
     }
 }
