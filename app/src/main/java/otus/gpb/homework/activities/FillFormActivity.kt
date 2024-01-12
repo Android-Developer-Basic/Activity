68e0f540288d7cd8d931c2f8b284989b9bc094b8
Alina <alinavik93@gmail.com>
2024-01-12 19:34:44 +0500

    дз 3

---
+++ app/src/main/java/otus/gpb/homework/activities/FillFormActivity.kt
@@ -0,0 +1,25 @@
+package otus.gpb.homework.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class FillFormActivity : AppCompatActivity(R.layout.activity_fill_form) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val editName = findViewById<EditText>(R.id.editName)
+        val editSurname = findViewById<EditText>(R.id.editSurname)
+        val editAge = findViewById<EditText>(R.id.editAge)
+
+        findViewById<Button>(R.id.buttonApply).setOnClickListener {
+            intent = Intent()
+                .putExtra("name", editName.text.toString())
+                .putExtra("surname", editSurname.text.toString())
+                .putExtra("age", editAge.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+}
