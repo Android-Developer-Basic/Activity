d57e82d33f5361623a94966bf34d9f61acaed44c
anastasiashchegoleva <a.shchegoleva88@mail.ru>
2024-05-21 20:40:50 +0300

    HomeWork Activity2

---
+++ mylibrary/src/main/java/com/example/mylibrary/Payload.kt
@@ -0,0 +1,11 @@
+package com.example.mylibrary
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Payload(
+    val title: String,
+    val year: String,
+    val description: String
+): Parcelable
