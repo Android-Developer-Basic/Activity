ad91277fcc08fbb1240cccb1daf759fbe45014b2
Ivan Toropygin <itori86@mail.ru>
2024-06-16 13:46:54 +0300

    hw_8 add SenderActivity

---
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -0,0 +1,12 @@
+package otus.gpb.homework.activities.sender
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import otus.gpb.homework.activities.receiver.R
+
+class SenderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sender)
+    }
+}
