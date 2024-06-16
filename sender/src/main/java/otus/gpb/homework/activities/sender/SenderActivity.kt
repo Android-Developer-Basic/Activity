25ee52d078e5e412bdb31bcdc28ad184c5994761
Ivan Toropygin <itori86@mail.ru>
2024-06-16 13:51:36 +0300

    hw_8 add buttons toGoogleMapsButton and sendEmailButton on SenderActivity

--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -1,6 +1,9 @@
 package otus.gpb.homework.activities.sender
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import otus.gpb.homework.activities.receiver.R
 
@@ -8,5 +11,26 @@ class SenderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sender)
+
+        val toGoogleMapsButton: Button = findViewById(R.id.btn_to_google_maps)
+        val sendEmailButton: Button = findViewById(R.id.btn_send_email)
+        val openReceiverButton: Button = findViewById(R.id.btn_open_receiver)
+
+        toGoogleMapsButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=Рестораны"))
+                .setPackage("com.google.android.apps.maps")
+            startActivity(intent)
+        }
+
+        sendEmailButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:android@otus.ru",))
+                .putExtra(Intent.EXTRA_SUBJECT,"Тема письма")
+                .putExtra(Intent.EXTRA_TEXT, "Текст письма")
+            startActivity(intent)
+        }
+
+        openReceiverButton.setOnClickListener {
+
+        }
     }
 }
